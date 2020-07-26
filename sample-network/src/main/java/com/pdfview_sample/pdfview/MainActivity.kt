2e4bb55581d6ad54e075a37bd60c821b50178870
Dmitry Borodin <git@DmitryBorodin.com>
2020-07-26 19:01:20 +0200

    viewmodel usage added

--- sample-network/src/main/java/com/pdfview_sample/pdfview/MainActivity.kt
+++ sample-network/src/main/java/com/pdfview_sample/pdfview/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pdfview_sample.pdfview
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.observe
 import com.pdfview.PDFView
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<PDFView>(R.id.activityMainPdfView).fromAsset("great-expectations.pdf").show()
+        pdfViewModel.getLoadedFile().observe(this) { url ->
+            findViewById<PDFView>(R.id.activityMainPdfView).fromFile(url).show()
+        }
+
     }
 }
